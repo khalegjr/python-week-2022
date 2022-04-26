1e2ffca63a5f697b9490d0230b505f08be6a2653
Khaled <khalegjr@gmail.com>
2022-04-26 19:50:36 -0300

    Day 2: Change API function to not access model directly

--- beerlog/api.py
+++ beerlog/api.py
@@ -1,9 +1,7 @@
 from typing import List, Optional
 from fastapi import FastAPI, Response, status # ASGI
 from beerlog.core import get_beers_from_database, add_beer_to_database
-from beerlog.models import Beer
 from beerlog.serializers import BeerIn, BeerOut
-from beerlog.database import get_session
 
 
 api = FastAPI(title="Beerlog")
@@ -20,13 +18,6 @@ def list_beers(style: Optional[str] = None):
 
 @api.post("/beers", response_model=BeerOut)
 def add_beer(beer_in: BeerIn, response: Response):
-    beer = Beer(**beer_in.dict())
-
-    with get_session() as session:
-        session.add(beer)
-        session.commit()
-        session.refresh(beer)
-
-    response.status_code = status.HTTP_201_CREATED
-
-    return beer
+    
+    if add_beer_to_database(**beer_in.dict()):
+        response.status_code = status.HTTP_201_CREATED
