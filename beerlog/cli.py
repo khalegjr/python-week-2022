30920ae5675ac05190619fd1369d8ef3322a39d1
Khaled <khalegjr@gmail.com>
2022-04-26 19:35:44 -0300

    Day 2: Implements CLI and backend

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,47 @@
-from .config import settings
+from email import header
+import typer
+from typing import Optional
+from beerlog.core import add_beer_to_database, get_beers_from_database
+from rich.table import Table
+from rich.console import Console
+
+main = typer.Typer(help="Beer Management Application")
+console = Console()
 
 
-def main():
-    print("Hello from", settings.NAME)
+@main.command("add")
+def add(
+    name: str,
+    style: str,
+    flavor: int = typer.Option(...),
+    image: int = typer.Option(...),
+    cost: int = typer.Option(...),
+):
+    """Adds a new beer to database
+
+    Args:
+        name (str): _description_
+        style (str): _description_
+    """
+    if add_beer_to_database(name, style, flavor, image, cost):
+        print(":beer: beer added to database")
+
+
+
+@main.command("list")
+def list_beers(style: Optional[str] = None):
+    """Lists beers in database
+
+    Args:
+        style (str): _description_
+    """
+    beers = get_beers_from_database(style)
+    table = Table(title="Beerlog :beer_mug:")
+    headers = ["id", "name", "style", "rate", "date"]
+    for header in headers:
+        table.add_column(header, style="magenta")
+    
+    for beer in beers:
+        values = [str(getattr(beer, header)) for header in headers]
+        table.add_row(*values)
+    console.print(table)
