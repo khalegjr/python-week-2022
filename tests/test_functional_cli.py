c7dd471b42f1cc94fbb1dc70119e54a11ccc3ca4
Khaled <khalegjr@gmail.com>
2022-04-27 17:18:45 -0300

    test: Create unit and functional tests

---
+++ tests/test_functional_cli.py
@@ -0,0 +1,31 @@
+from unittest import result
+from typer.testing import CliRunner
+
+from beerlog.cli import main
+
+runner = CliRunner()
+
+
+def test_add_beer():
+    result = runner.invoke(
+        main, ["add", "Skol", "KornPA", "--flavor=1", "--image=1", "--cost=2"]
+    )
+    assert result.exit_code == 0
+    assert "beer added" in result.stdout
+
+def test_list_beer():
+    # Arrange
+    runner.invoke(
+        main, ["add", "Skol", "KornPA", "--flavor=1", "--image=1", "--cost=2"]
+    )
+
+    # Act
+    result = runner.invoke(
+        main, ["list", "--style", "Witbier"]
+    )
+
+    # Assert
+    assert result.exit_code == 0
+
+    # TODO: Implementar a verificação dentro da tabela de saída
+    # assert "Witbier" in result.stdout
